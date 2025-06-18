635911d96993dc05367cbf8417f3c9a6ae8905d2
Shreya <shreyaalpha1@gmail.com>
2025-06-18 08:41:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,48 +2,48 @@ let roll = document.querySelector("button");
 roll.addEventListener("click", ()=> {
 var randomNum = (Math.floor(Math.random() * 6)) + 1;
 if(randomNum===1){
-document.querySelector(".img1").src="../ProjectDiceGame/image/dice1.png";
+document.querySelector(".img1").src="./image/dice1.png";
 }
 else if(randomNum===2){
-    document.querySelector(".img1").src="../ProjectDiceGame/image/dice2.png";
+    document.querySelector(".img1").src="./image/dice2.png";
 }
 else if(randomNum===3){
-    document.querySelector(".img1").src="../ProjectDiceGame/image/dice3.png";
+    document.querySelector(".img1").src="./image/dice3.png";
 }
 else if(randomNum===4){
-    document.querySelector(".img1").src="../ProjectDiceGame/image/dice4.png";
+    document.querySelector(".img1").src="./image/dice4.png";
 }
 else if(randomNum===5){
-    document.querySelector(".img1").src="../ProjectDiceGame/image/dice5.png";
+    document.querySelector(".img1").src="./image/dice5.png";
 }
 else if(randomNum===6){
-    document.querySelector(".img1").src="../ProjectDiceGame/image/dice6.png";
+    document.querySelector(".img1").src="./image/dice6.png";
 }
 else{
-    document.querySelector(".img1").src="../ProjectDiceGame/image/dice6.png";
+    document.querySelector(".img1").src="./image/dice6.png";
 }
 
 var randomNum2 = (Math.floor(Math.random()*6)) +1 ;
 if(randomNum2===1){
-    document.querySelector(".img2").src="../ProjectDiceGame/image/dice1.png";
+    document.querySelector(".img2").src="./image/dice1.png";
     }
     else if(randomNum2===2){
-        document.querySelector(".img2").src="../ProjectDiceGame/image/dice2.png";
+        document.querySelector(".img2").src="./image/dice2.png";
     }
     else if(randomNum2===3){
-        document.querySelector(".img2").src="../ProjectDiceGame/image/dice3.png";
+        document.querySelector(".img2").src="./image/dice3.png";
     }
     else if(randomNum2===4){
-        document.querySelector(".img2").src="../ProjectDiceGame/image/dice4.png";
+        document.querySelector(".img2").src="./image/dice4.png";
     }
     else if(randomNum2===5){
-        document.querySelector(".img2").src="../ProjectDiceGame/image/dice5.png";
+        document.querySelector(".img2").src="./image/dice5.png";
     }
     else if(randomNum2===6){
-        document.querySelector(".img2").src="../ProjectDiceGame/image/dice6.png";
+        document.querySelector(".img2").src="./image/dice6.png";
     }
     else{
-        document.querySelector(".img2").src="../ProjectDiceGame/image/dice6.png";
+        document.querySelector(".img2").src="./image/dice6.png";
     }
     if(randomNum>randomNum2){
         document.querySelector("h1").innerHTML = "👑Player1 Wins";
